a6c3e8db40aa1ebc938f94d5605d2a0ac9b4ddf5
Karol <karolzymkowski1990@gmail.com>
2024-04-17 22:05:10 +0200

    Initial commit

--- src/XCopy.java
+++ src/XCopy.java
@@ -1,15 +1,17 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class XCopy {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int org = 42;
+        XCopy x = new XCopy();
+
+        int y = x.drive(org);
+        System.out.println(org + "" + y);
+
     }
+        int drive(int arg) {
+        arg = arg * 2;
+        return arg;
+        }
 }
